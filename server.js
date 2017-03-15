5352990824db5137a60ad2dbc193d5d4315faa5e
Joseph Davis <soccermaniac898@gmail.com>
2017-03-15 17:56:45 -0500

    Add service

--- server.js
+++ server.js
@@ -7,7 +7,38 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-   res.send("It works!");
+    var file = path.join(__dirname, "index.html");
+
+    res.sendFile(file, function(err){
+        if (err) {
+            res.statusCode = err.status;
+            console.log("Couldn't serve file: " + file);
+        } else {
+            console.log("Serving file: " + file);
+        }
+    })
+});
+
+app.get('/:date', function(req, res){
+   var date = req.params.date;
+
+   if (!isNaN(date)) {
+       date = moment(date, "X");
+   } else {
+       date = moment(date, "MMMM D, YYYY");
+   }
+
+   if (date.isValid()) {
+       res.json({
+           "unix": date.format("X"),
+           "natural": date.format("MMMM D, YYYY")
+       });
+   } else {
+       res.json({
+           "unix": null,
+           "natural": null
+       })
+   }
 });
 
 app.listen(port, function(){
