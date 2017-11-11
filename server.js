d590367d3bc67ab5ee82792190d6c2d3cdef736b
Joseph Davis <soccermaniac898@gmail.com>
2017-11-11 10:52:55 -0600

    Update server file

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
-var express = require('express');
-var moment = require('moment');
-var path = require('path');
+const express = require('express');
+const moment = require('moment');
+const path = require('path');
 
-var app = express();
+const app = express();
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    var file = path.join(__dirname, "index.html");
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-    res.sendFile(file, function(err){
-        if (err) {
-            res.statusCode = err.status;
-            console.log("Couldn't serve file: " + file);
-        } else {
-            console.log("Serving file: " + file);
-        }
-    })
-});
-
-app.get('/:date', function(req, res){
-   var date = req.params.date;
+app.get('/:date', (req, res) => {
+   let date = req.params.date;
 
    if (!isNaN(date)) {
        date = moment(date, "X");
@@ -37,10 +26,8 @@ app.get('/:date', function(req, res){
        res.json({
            "unix": null,
            "natural": null
-       })
+       });
    }
 });
 
-app.listen(port, function(){
-    console.log("App listening on port " + port);
-});
+app.listen(port, () => console.log(`App now listening on port ${port}...`));
